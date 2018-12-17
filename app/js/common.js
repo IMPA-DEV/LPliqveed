ca82c01000d663108d6c61740334faa01acc9723
MAXON-BY <maxon.pro2015@gmail.com>
2018-12-17 18:21:27 +0300

    сделана мобилка + слайдер + page to scroll

--- app/js/common.js
+++ app/js/common.js
@@ -1,8 +1,27 @@
 $(function() {
 
-    console.log('Трынь...');
-    console.log('Тр...');
-    console.log('Тр...');
-    console.log('ПЫЫЫХ и завелось...');
+    //Slick slider
+    $('.slider-effects').slick({
+        infinite: true,
+        speed: 800,
+        dots: true,
+        autoplay: true,
+        autoplaySpeed: 2500,
+        fade: true,
+        cssEase: 'linear',
+        arrows: false
+    });
+
+    // Mob menu
+    $('.header-mob, .close-nav').click(function () {
+        $('.header-nav').toggleClass('mobile');
+        $('body').toggleClass('vh');
+        $('.close').show(0);
+    })
+
+    // Page to scroll
+    $(".header-nav a").mPageScroll2id();
 
 });
+
+
